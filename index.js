173a57d7f2768a62df22fbd68cacaca4847cefec
deniseFa <38798496+deniseFa@users.noreply.github.com>
2023-12-11 00:07:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,7 @@ app.use(upload.array('imagenes', 5)); // Multer para manejar campos de archivos
 app.use(session({
   secret: 'secreto_5112634128dfa',
   resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 3600000 } // 1 hora (en milisegundos)
+  saveUninitialized: false
 }));
 
 // Conexión a la base de datos con mysql2
