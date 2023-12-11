5f26e978888b46866a99828c257166e75ef4a2ea
deniseFa <38798496+deniseFa@users.noreply.github.com>
2023-12-11 01:13:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser = require('body-parser');
 const session = require('express-session');
-const multer = require('multer'); // Agregamos multer para manejar archivos
+const multer = require('multer');
 const databaseConfig = require('./src/config/database');
+
+// Conexión a la base de datos con mysql2
+const { conn } = require('./src/config/database');
+
+// Middleware para agregar la conexión a cada solicitud
+app.use((req, res, next) => {
+  req.mysql = conn;
+  next();
+});
 
 // Configuración de EJS como motor de plantillas
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 
-// Conexión a la base de datos con mysql2
-const { conn } = require('./src/config/database');
-
-// Configuración de la sesión
-app.use(session({
-  secret: 'secreto_5112634128dfa',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: false, //
-    sameSite: 'strict', // Cambiado a 'strict'
-    maxAge: 3600000, // 1 hora
-  },
-}));
-
-
 // Configuración de multer para manejar archivos
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, 'public/uploads'); // Ruta donde se guardarán las imágenes
+    callback(null, 'public/uploads');
   },
   filename: (req, file, callback) => {
-    // Lógica para asignar nombres a los archivos, si es necesario
     const extension = file.mimetype.split('/')[1];
     const nombreImagen = `${Date.now()}.${extension}`;
     callback(null, nombreImagen);
@@ -44,16 +35,19 @@ const upload = multer({ storage });
 // Configuración de body-parser para analizar el cuerpo de las solicitudes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(upload.array('imagenes', 5)); // Multer 
+app.use(upload.array('imagenes', 5));
 
-// Middleware para agregar la conexión a cada solicitud
-app.use((req, res, next) => {
-  req.mysql = conn;
-  next();
-});
-
-// Configuración para servir archivos estáticos desde las carpetas 'public' y 'src/public'
-app.use(express.static(path.join(__dirname, 'public')));
+// Configuración de la sesión
+app.use(session({
+  secret: 'secreto_5112634128dfa',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    sameSite: 'strict',
+    maxAge: 3600000,
+  },
+}));
 
 // Rutas de la aplicación
 app.use('/', require('./src/routes/index'));
