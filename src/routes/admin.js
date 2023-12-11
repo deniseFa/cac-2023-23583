6f7bffd1c6d503c4d8ed97465d2be11151eb447f
deniseFa <38798496+deniseFa@users.noreply.github.com>
2023-12-11 01:27:33 -0300

    Update admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,22 +6,23 @@ const createProductController = require('../controllers/createProductController'
 const editProductController = require('../controllers/editProductController');
 const deleteProductController = require('../controllers/deleteProductController');
 
-const authMiddleware = require('../middlewares/authMiddleware'); // Importa el middleware de autenticación
+// Comentado para desactivar temporalmente la validación de autenticación
+// const authMiddleware = require('../middlewares/authMiddleware'); // Importa el middleware de autenticación
 
 // Ruta protegida por autenticación
-router.get('/dashboard', authMiddleware, dashboardController.getProducts);
+router.get('/dashboard', /*authMiddleware,*/ dashboardController.getProducts);
 // Manejar solicitudes POST a /admin/dashboard (para la búsqueda)
-router.post('/dashboard', authMiddleware, dashboardController.getProducts);
+router.post('/dashboard', /*authMiddleware,*/ dashboardController.getProducts);
 
 // Ruta para mostrar el formulario de creación de producto
-router.get('/createProduct', authMiddleware, createProductController.showCreateForm);
+router.get('/createProduct', /*authMiddleware,*/ createProductController.showCreateForm);
 
 // Ruta para procesar el formulario de creación de producto
-router.post('/createProduct', authMiddleware, createProductController.processCreateForm);
+router.post('/createProduct', /*authMiddleware,*/ createProductController.processCreateForm);
 
-router.get('/editProduct/:id', authMiddleware, editProductController.showEditForm);
-router.post('/editProduct/:id', authMiddleware, editProductController.processEditForm);
+router.get('/editProduct/:id', /*authMiddleware,*/ editProductController.showEditForm);
+router.post('/editProduct/:id', /*authMiddleware,*/ editProductController.processEditForm);
 
-router.post('/deleteProduct/:id', authMiddleware, deleteProductController.processDelete);
+router.post('/deleteProduct/:id', /*authMiddleware,*/ deleteProductController.processDelete);
 
 module.exports = router;
