63951d80b1333785b33b4b62ded88205c9bb0262
deniseFa <38798496+deniseFa@users.noreply.github.com>
2023-12-11 00:59:38 -0300

    Update authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,12 +1,20 @@
 // authMiddleware.js
 const authMiddleware = (req, res, next) => {
+  console.log('Middleware de autenticación llamado');
+  console.log('Sesión:', req.session);
+
   // Verifica si hay un usuario en la sesión
   if (!req.session || !req.session.user) {
-    return res.redirect('/auth/login'); // O redirige a la página de inicio de sesión
+    console.log('Usuario no autenticado. Redirigiendo...');
+    return res.redirect('http://localhost:3000/auth/login'); // O redirige a la página
   }
+
   // Usuario autenticado, permite el acceso
+  console.log('Usuario autenticado. Continuando...');
   next();
 };
 
-
 module.exports = authMiddleware;
+
+
+
