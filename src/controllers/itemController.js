dedb18762d600fbae84b07245eaec5d2a7ebc91b
deniseFa <38798496+deniseFa@users.noreply.github.com>
2023-12-13 17:21:06 -0300

    Add files via upload

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -1,32 +1,50 @@
 // itemController.js
 const { conn } = require('../config/database');
 
-// Función para obtener productos con información de la categoría y la licencia desde la base de datos
-const getProductosFromDB = async (limit = 6) => {
+// Función para obtener detalles de un producto por su ID desde la base de datos
+const getProductoByIdFromDB = async (productId) => {
   try {
-    const [rows, fields] = await conn.execute(`
-      SELECT p.*, l.licence_name
-      FROM product p
-      JOIN licence l ON p.licence_id = l.licence_id
-    `);
-    
-    // Limitar la cantidad de productos
-    const limitedProductos = rows.slice(0, limit);
+    const [productRows, productFields] = await conn.execute(
+      'SELECT p.*, l.licence_name FROM product p JOIN licence l ON p.licence_id = l.licence_id WHERE p.product_id = ?',
+      [productId]
+    );
 
-    return limitedProductos;
+    if (productRows.length > 0) {
+      const producto = productRows[0]; // Devuelve el primer (y único) producto encontrado
+
+      // Consulta para obtener productos relacionados
+      const [relatedRows, relatedFields] = await conn.execute(
+        'SELECT p.*, l.licence_name FROM product p JOIN licence l ON p.licence_id = l.licence_id WHERE p.licence_id = ? AND p.product_id != ? LIMIT 3',
+        [producto.licence_id, productId]
+      );
+
+      const productosRelacionados = relatedRows;
+
+      return { producto, productosRelacionados };
+    } else {
+      return null; // Si no se encuentra el producto, puedes manejarlo de la manera que desees
+    }
   } catch (error) {
     console.error('Error al ejecutar la consulta:', error.message);
     throw error;
   }
 };
 
-// Controlador para la página de items
-exports.getItem = async (req, res) => {
+// Controlador para la página de detalle del producto
+exports.getItemById = async (req, res) => {
+  const productId = req.params.id; // El ID del producto provisto en la URL
+
   try {
-    const productos = await getProductosFromDB();
-    res.render('item', { productos }); // Asegúrate de pasar { productos }
+    const { producto, productosRelacionados } = await getProductoByIdFromDB(productId);
+
+    if (producto) {
+      res.render('item', { producto, productosRelacionados });
+    } else {
+      // Manejar el caso donde el producto no se encuentra
+      res.status(404).send('Producto no encontrado');
+    }
   } catch (error) {
-    console.error('Error al obtener productos:', error.message);
+    console.error('Error al obtener detalles del producto:', error.message);
     res.status(500).send('Error interno del servidor');
   }
 };
