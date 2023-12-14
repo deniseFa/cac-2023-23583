e3a9a388401372f5beaf836680b37d2d05ce4617
deniseFa <38798496+deniseFa@users.noreply.github.com>
2023-12-14 19:50:00 -0300

    Update createProductController.js

--- src/controllers/createProductController.js
+++ src/controllers/createProductController.js
@@ -3,10 +3,10 @@ const createProductController = {};
 const fs = require('fs');
 const path = require('path');
 
-// Mostrar el formulario para crear un nuevo producto
+// formulario nuevo producto
 createProductController.showCreateForm = async (req, res) => {
   try {
-    // Obtener categorías y licencias desde la base de datos
+    // categorías y licencias
     const [categorias] = await req.mysql.query('SELECT * FROM category');
     const [licencias] = await req.mysql.query('SELECT * FROM licence');
 
@@ -20,14 +20,12 @@ createProductController.showCreateForm = async (req, res) => {
 // Procesar el formulario para crear un nuevo producto
 createProductController.processCreateForm = async (req, res) => {
   try {
-    // Obtener datos del formulario
     const { categoria, licencia, nombre, descripcion, sku, precio, stock, descuento, cuotas } = req.body;
 
-    // Utilizar un nombre fijo para las imágenes
     const imagenFront = '/uploads/comming-soon.webp';
     const imagenBack = '/uploads/comming-soon.webp';
 
-    // Guardar los datos en la base de datos
+    // base de datos
     const query = `
       INSERT INTO product (product_name, product_description, price, stock, discount, sku, dues, licence_id, category_id, image_front, image_back)
       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
@@ -35,7 +33,6 @@ createProductController.processCreateForm = async (req, res) => {
 
     await req.mysql.query(query, [nombre, descripcion, precio, stock, descuento, sku, cuotas, licencia, categoria, imagenFront, imagenBack]);
 
-    // Redirigir a la página del panel de administración después de crear el producto
     res.redirect('/admin/dashboard');
   } catch (error) {
     console.error('Error al procesar el formulario:', error.message);
